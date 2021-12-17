5df02e4cee16bbd6b891428d6e5dfa3bb823c262
Andrew Yan <git@andrewyan.dev>
2021-12-17 14:39:16 -0800

    Refactor different test cases

--- app.test.js
+++ app.test.js
@@ -3,11 +3,9 @@ const request = require('supertest');
 const app = require('./app');
 
 const factorial = (n) => {
-  if (n == 1) { return 1; }
-  return n * factorial(n - 1);
+  if (n == 1) { return 1n; }
+  return BigInt(n) * factorial(n - 1);
 };
-
-const FACTORIAL_10 = factorial(10).toString();
 
 describe('GET /benchmark/:n', () => {
   test('should benchmark factorial 10', async () => {
@@ -18,29 +16,29 @@ describe('GET /benchmark/:n', () => {
     const strings = res.text.split(' ');
     const singleResult = strings[3];
     const multiResult = strings[14];
-    expect(singleResult).toEqual(FACTORIAL_10);
+    expect(singleResult).toEqual(factorial(10).toString());
     expect(singleResult).toEqual(multiResult);
   });
 });
 
 describe('GET /thread/:n', () => {
-  test('should calculate factorial 10', async () => {
+  test('should calculate factorial 14', async () => {
     const res = await request(app)
-      .get('/thread/10')
+      .get('/thread/14')
       .expect(200);
 
     const factorialResult = res.text.split(' ')[2];
-    expect(factorialResult).toEqual(FACTORIAL_10);
+    expect(factorialResult).toEqual(factorial(14).toString());
   });
 });
 
 describe('GET /threads/:n', () => {
-  test('should calculate factorial 10', async () => {
+  test('should calculate factorial 99', async () => {
     const res = await request(app)
-      .get('/threads/10')
+      .get('/threads/99')
       .expect(200);
 
     const factorialResult = res.text.split(' ')[2];
-    expect(factorialResult).toEqual(FACTORIAL_10);
+    expect(factorialResult).toEqual(factorial(99).toString());
   });
 });
